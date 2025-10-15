088ed88894a408b9755a8a6121d107763ebef579
ronak-patel-xcodeweb <ronak@xcodewebsolutions.com>
2025-10-15 22:45:59 +0530

    Add changes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import ChildLayout from "./page";
 import { authOptions } from "@/lib/auth";
+import ChildLayout from "./child-layout";
 
 export default async function ProtectedLayout({ children }: { children: ReactNode }) {
   const session = await getServerSession(authOptions);
-  
+ 
   if (!session) {
     redirect("/login");
   }
